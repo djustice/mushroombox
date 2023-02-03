2d6ae7ac7d9f5e8924e28a86e7c92c27c813be89
Dustin Ricketts <djustice.kde@gmail.com>
2023-02-03 00:28:36 -0500

    add shed items, adjust various positions

--- Assets/Scripts/Shed/ShedClickable.cs
+++ Assets/Scripts/Shed/ShedClickable.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,32 +18,63 @@ public class ShedClickable : MonoBehaviour
     {
         this.D("shed clickable click: " + id);
 
-        if (Game.coinCount >= 30)
-        {
-            if (bubble.gameObject.activeSelf == false)
-            {
-                bubble.gameObject.SetActive(true);
-                StartCoroutine("DelayCloseBubble");
-                return;
-            }
-            else 
-            {
-                this.D("shed item: " + id + " paid");
-                Game.counter.coinChange(-30);
-                Game.goalDisplays[0].goal.Value = Game.goalDisplays[0].goal.Value + 1;
-                Game.goalDisplays[0].RefreshData();
-                Game.shedItems[id] = false;
-                if (Game.goalDisplays[0].goal.Value == 16) 
-                {
-                    // todo: show end of game dialog
-                    GameObject.Find("EndDialog").GetComponent<Animator>().SetBool("Idle", false);
-                    GameObject.Find("EndDialog").GetComponent<Animator>().SetBool("Extend", true);
-                }
-                SaveSystem.SaveGame();
-                gameObject.SetActive(false);
-                bubble.gameObject.SetActive(false);
-            }
-        }
+	    if (id <= 16) {
+	        if (Game.coinCount >= 30)
+	        {
+	            if (bubble.gameObject.activeSelf == false)
+	            {
+	                bubble.gameObject.SetActive(true);
+	                StartCoroutine("DelayCloseBubble");
+	                return;
+	            }
+	            else 
+	            {
+		            this.D("shed item: " + id + " paid");
+	                Game.counter.coinChange(-30);
+	                Game.goalDisplays[0].goal.Value = Game.goalDisplays[0].goal.Value + 1;
+	                Game.goalDisplays[0].RefreshData();
+	                Game.shedItems[id] = false;
+		            if (Game.goalDisplays[0].goal.Value == 19) 
+	                {
+	                    // todo: show end of game dialog
+	                    GameObject.Find("EndDialog").GetComponent<Animator>().SetBool("Idle", false);
+	                    GameObject.Find("EndDialog").GetComponent<Animator>().SetBool("Extend", true);
+	                }
+	                SaveSystem.SaveGame();
+	                gameObject.SetActive(false);
+	                bubble.gameObject.SetActive(false);
+	            }
+	        }
+	    }
+	    else
+	    {
+		    if (Game.coinCount >= 100)
+		    {
+			    if (bubble.gameObject.activeSelf == false)
+			    {
+				    bubble.gameObject.SetActive(true);
+				    StartCoroutine("DelayCloseBubble");
+				    return;
+			    }
+			    else
+			    {
+				    this.D("shed item: " + id + " paid");
+				    Game.counter.coinChange(-100);
+				    Game.goalDisplays[0].goal.Value = Game.goalDisplays[0].goal.Value + 1;
+				    Game.goalDisplays[0].RefreshData();
+				    Game.shedItems[id] = false;
+				    if (Game.goalDisplays[0].goal.Value == 19) 
+				    {
+					    // todo: show end of game dialog
+					    GameObject.Find("EndDialog").GetComponent<Animator>().SetBool("Idle", false);
+					    GameObject.Find("EndDialog").GetComponent<Animator>().SetBool("Extend", true);
+				    }
+				    SaveSystem.SaveGame();
+				    gameObject.SetActive(false);
+				    bubble.gameObject.SetActive(false);
+			    }
+		    }
+	    }
     }
 
     public IEnumerator DelayCloseBubble()
